e94d13344196984ce718307949b06be103465cbf
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-13 22:30:44 +0530

    Add files via upload

---
+++ Ternary-2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define pf printf
+#define sf scanf
+
+void main()
+{
+	short x,y;
+	
+	printf("enter value of x :");
+	sf("%hi,&x");
+	
+	printf("enter value of y :");
+	sf("%hi,&y");
+	
+	(x>y)?pf ("x is maximu"): pf ("y is maximum");
+	
+	
+	
+	
+}
