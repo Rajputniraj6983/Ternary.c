63429420796d60590b0e94e6d45949f80d4a3f17
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-14 21:36:20 +0530

    Add files via upload

---
+++ Switch-day.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#define pf printf
+#define sf scanf
+void mian(){
+	
+		char day;
+
+	pf("\nEnter alphabet : ");
+	sf("%c",&day);
+		
+		switch(day){
+		
+		case 'M':
+			pf("Monday");
+			break;
+		
+		case 'T':
+			pf("Tuesday");
+			break;
+			
+		case 'W':
+			pf("Wednesday");
+			break;
+			
+		case 't':
+			pf("Thursday");
+			break;
+			
+		case 'F':
+			pf("Friday");
+			break;
+			
+		case 'S':
+			pf("Saturday");
+			break;
+			
+		case 's':
+			pf("Sunday");
+			break;
+			
+			
+		default:
+			pf("Invalid alphabet!");
+		
+		}
+		
+		}
+	
+
+	
