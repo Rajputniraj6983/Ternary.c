3eb2de5a356cfb984c749d42e715a68dd05289b3
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-14 16:51:05 +0530

    Add files via upload

---
+++ Odd-Even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define pf printf
+#define sf scanf
+
+void main(){
+	
+	
+	int x;
+    
+	printf("enter the value of x !");
+	sf("%d",&x);
+	
+	
+//	(x % 2 ==0) ? pf("%hi is even number",x) : pf("%hi is odd number",x);
+	
+	
+}
