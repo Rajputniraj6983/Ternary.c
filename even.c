949f5667e3d958285eb8a3d4597deaa0ce1d229e
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-15 21:44:30 +0530

    15-09-2023

---
+++ even.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int N, x = 1;
+	printf("enter the value of x ");
+	scanf("%d",&N);
+	
+	while(x <= N);{
+       
+	   	if("x % 2 != 0");
+	  
+     	  printf("%d",x);
+	      x++;
+	
+}
+	
+	
+	
+	
+}
