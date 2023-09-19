c10168fe36cdc70058a1295b015d9739943a96b1
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-19 10:02:45 +0530

    18-09-2023

---
+++ forodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	
+	short n,x;
+	printf("enter the value of n : ");
+	scanf("%hi",&n);
+	for(x = 1; x <= n ; x++){
+		
+		
+		if(x % 2 != 0)
+		 printf("%hi", x);
+	}
+	
+	
+	
+	
+}
