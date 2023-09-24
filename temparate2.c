67ebd7e2d6f1e2b8dacc1b2def9d3eebc034ffd9
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-24 19:42:17 +0530

    24-09-2023

---
+++ temparate2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	int x,n;
+	{
+		printf("enter the value of n : ");
+		scanf("%d",&n);
+		for(x = 1;x <=10;x++){
+	    printf("%d*%d=%d\n",n,x,(n*x));
+		}
+	}
+	
+	
+	
+	
+	
+	
+}
