69420f5754d62946b17e0b22e006865351bd366b
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-09-27 11:41:39 +0530

    27-09-2023

---
+++ patterm6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+	short i, j, z;
+	
+	for(i = 1; i <= 5; i++){
+		
+	for(z = 5; z > i; z--){
+		
+		printf(" ");
+	}	
+	 for(j = 1; j <= i; j++){
+	 	
+	 }	
+	    printf("\n");
+	
+	}
+
+}
