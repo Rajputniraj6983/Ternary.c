bc1a86b0406822218e57cf1afca7ac09004d4757
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-10-01 19:53:34 +0530

    01-10-2023

---
+++ p1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	int x,y,z;
+	for(x = 1;x <= 5;x++)
+	for(z = 1;z <= 5;x++)
+{
+	for(z = 1;z <= 5;z++);
+
+{
+
+	printf(" ");
+
+  }  
+		
+		for(y = 1;y <= 5;y++)
+}
+printf("\n");
+	
+	
+	
+	
+	
+}
