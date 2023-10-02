308db509f50366e110f3732bd04dc110a0c0e7d5
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-10-02 22:20:58 +0530

    02-10-2023

---
+++ q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+	short n;
+	float average = 0;
+	
+	printf("Enter size of array : ");
+	scanf("%hi", &n);
+	
+	short a[n];
+	
+	printf("Enter array elements:\n");
+	
+	for (short i = 0; i < n; i++){
+	
+		printf("enter value of a[%hi] : ",i);
+		scanf("%hi", &a[i]);
+		average += a[i];	
+	}
+	
+	printf("\n\nAverage of an array : %.1f", average / n);
+
+}
