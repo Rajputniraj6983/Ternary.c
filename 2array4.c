9b42b9ba7ac42549a755d368908d1ac1ce19a9e9
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-10-05 09:24:06 +0530

    05-10-2023

---
+++ 2array4.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+void main(){
+
+	short n, m, sum;
+	
+	
+
+	printf("Enter size of row : ");
+	scanf("%hi", &n);
+
+	printf("Enter size of column : ");
+	scanf("%hi", &m);
+	
+
+	short a[n][m];
+	short sum_arr[n];
+	
+	for (short i = 0; i < n; i++){
+		
+		sum = 0;
+		for (short j = 0; j < m; j++){
+		
+			printf("\nEnter value of a[%hi][%hi] : ", i, j);
+			scanf("%hi", &a[i][j]);
+			sum += a[i][j];
+			
+		}
+		
+		sum_arr[i] = sum;
+	
+	}
+	
+	
+	printf("\n\nCreated array:\n");
+	for (short i = 0; i < n; i++){
+	
+		for (short j = 0; j < m; j++){
+		
+			printf("%hi ", a[i][j]);
+			
+		}
+		printf("\n");
+	
+	}
+	
+	printf("\n\nRow-wise sum of array:\n");
+	for (short i = 0; i < n; i++)
+		printf("Row %hi sum : %hi \n", i+1, sum_arr[i]);
+		
+	
+}
