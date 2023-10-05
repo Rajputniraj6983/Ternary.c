b7a4eb3ecae49aca6b482509764685c615f1bc79
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-10-05 09:23:15 +0530

    05-10-2023

---
+++ 2darray2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main(){
+
+	short n, m, sum = 0;
+	
+	
+
+	printf("Enter size of row : ");
+	scanf("%hi", &n);
+
+	printf("Enter size of column : ");
+	scanf("%hi", &m);
+	
+
+	short a[n][m];
+	
+	for (short i = 0; i < n; i++){
+	
+		for (short j = 0; j < m; j++){
+		
+		printf("\nEnter value of a[%hi][%hi] : ", i, j);
+		scanf("%hi", &a[i][j]);
+		sum += a[i][j];
+		
+		}
+	
+	}
+	
+	
+	printf("\n\nSum of 2D array : %d", sum);
+	
+}
