3376827a3e23a8ab387c6e1af97a0c74fd4dd218
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-11-03 22:53:55 +0530

    0.3-11-2023

---
+++ filament1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[20];
+    
+    puts("Enter the string : ");
+    gets(str);
+
+    puts("\n \n");
+    
+    short length = 0, i, j = 0;
+    
+    for ( i = 0; str[i] != '\0'; i++)
+        length++;
+        
+    char str2[length - 1];
+    
+    for ( i = (length - 1) ; i >= 0 ; i--){
+        
+        str2[j] = str[i];
+        j++;
+        
+    }
+    
+    puts("Reversed string is : ");
+    puts(str2);
+    
+    if ( strcmp(str, str2))
+        puts("\nstring is not palindrome");
+    else
+        puts("\nstring is palindrome");
+}
