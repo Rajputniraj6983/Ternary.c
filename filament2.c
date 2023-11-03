c3025c636c9680bdc0ef5cdc79a1d5a79279b277
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-11-03 22:54:22 +0530

    03-11-2023

---
+++ filament2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+
+	char str[20], str2[20];
+	
+	printf("Enter String1 : ");
+	gets(str);
+	
+	
+	strcpy(str2, str);
+	
+	printf("\nString2 : %s", str);
+
+	printf("\nString2 : %s", str2);
+
+}
