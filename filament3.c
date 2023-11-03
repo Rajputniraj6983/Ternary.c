36a273ff35cfdd285eec35bf25490115f2000d6d
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2023-11-03 22:54:45 +0530

    03-11-2023

---
+++ filament3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+
+	char str[20], str2[20];
+	
+	printf("Enter String1 : ");
+	gets(str);
+	
+	printf("Enter String2 : ");
+	gets(str2);
+	
+	
+	if (strcmp(str, str2)){
+	
+		puts("\n\nBoth string are not same");
+	
+	} else {
+	
+		puts("\n\nBoth string are same");
+	
+	}
+
+}
